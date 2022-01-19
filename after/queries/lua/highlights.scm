5fc4e033378892c9ac6ebc0c6aa4a749b9ce1e76
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-01-19 10:12:52 +0530

    update lua queries

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -5,7 +5,8 @@
 
 (assignment_statement
   (variable_list
-    name: (dot_index_expression) @alabaster.definition)
+    name: (dot_index_expression
+            field: (identifier) @alabaster.definition))
   (expression_list
     value: (function_definition)))
 
@@ -16,7 +17,8 @@
     value: (function_definition)))
 
 (function_declaration
-  name: (dot_index_expression) @alabaster.definition)
+  name: (dot_index_expression
+          field: (identifier) @alabaster.definition))
 
 (table_constructor
   (field name: (identifier) @alabaster.string))
