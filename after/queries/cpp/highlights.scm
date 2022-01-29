6ec67429b8cf583e889bd6caeeb03225ee902e4c
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-01-29 10:53:40 +0530

    more c++ captures

--- after/queries/cpp/highlights.scm
+++ after/queries/cpp/highlights.scm
@@ -10,6 +10,20 @@
   declarator: (function_declarator
                 declarator: (field_identifier) @alabaster.definition))
 
+(function_definition
+  declarator: (function_declarator
+                declarator: (qualified_identifier
+                              name: (destructor_name
+                                      (identifier) @alabaster.definition))))
+
+(declaration
+  declarator: (function_declarator
+                declarator: (destructor_name (identifier) @alabaster.definition)))
+
+(field_declaration
+  declarator: (function_declarator
+                declarator: (field_identifier) @alabaster.definition))
+
 (labeled_statement
   label: (statement_identifier) @alabaster.definition)
 
