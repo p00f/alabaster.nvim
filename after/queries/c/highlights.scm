d0f8ed0bc52f7d5368d92e9921d65adf94a404d6
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-06-18 21:43:11 +0530

    update c queries

--- after/queries/c/highlights.scm
+++ after/queries/c/highlights.scm
@@ -4,5 +4,16 @@
   declarator: (identifier) @alabaster.definition)
 (preproc_function_def
   name: (identifier) @alabaster.definition)
-(struct_specifier
-  name: (type_identifier) @alabaster.definition)
+
+(type_definition
+  type: (struct_specifier
+          name: (type_identifier) @alabaster.definition)
+  declarator: (type_identifier) @alabaster.definition)
+
+(type_definition
+  type: (struct_specifier)
+  declarator: (type_identifier) @alabaster.definition)
+
+(type_definition
+  type: (enum_specifier)
+  declarator: (type_identifier) @alabaster.definition)
