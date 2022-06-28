1f539915854db79e4a7e2b6141e092d9aebee430
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-06-28 19:03:29 +0530

    add kotlin queries

---
+++ after/queries/kotlin/highlights.scm
@@ -0,0 +1,8 @@
+;; vim: ft=query
+(function_declaration
+  (simple_identifier) @alabaster.definition)
+
+(class_declaration
+  (type_identifier) @alabaster.definition)
+
+(modifiers (annotation (user_type (type_identifier) @alabaster.punctuation)))
