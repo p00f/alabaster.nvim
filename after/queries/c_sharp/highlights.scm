49864f478a2d33fdea93ca3523c475a7cc54b168
Zorbn <public.zorb@gmail.com>
2023-08-10 16:17:19 -0700

    add c_sharp highlights

---
+++ after/queries/c_sharp/highlights.scm
@@ -0,0 +1,25 @@
+;; vim: ft=query
+;; extends
+
+(class_declaration
+  name: (identifier) @AlabasterDefinition)
+(struct_declaration
+  name: (identifier) @AlabasterDefinition)
+(interface_declaration
+  name: (identifier) @AlabasterDefinition)
+(record_declaration
+  name: (identifier) @AlabasterDefinition)
+(record_struct_declaration
+  name: (identifier) @AlabasterDefinition)
+(constructor_declaration
+  name: (identifier) @AlabasterDefinition)
+(destructor_declaration
+  name: (identifier) @AlabasterDefinition)
+(method_declaration
+  name: (identifier) @AlabasterDefinition)
+(property_declaration
+  name: (identifier) @AlabasterDefinition)
+(enum_declaration
+  name: (identifier) @AlabasterDefinition)
+(namespace_declaration
+  name: (identifier) @AlabasterDefinition)
