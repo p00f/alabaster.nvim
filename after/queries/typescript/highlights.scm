2814d4ef76850f686d52d00dd963f88605f61b08
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-17 23:03:12 +0100

    improved typescript highlighting

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -17,4 +17,23 @@
 (type_alias_declaration
   name: (type_identifier) @AlabasterDefinition)
 
+;; class declarations
+(class_declaration
+  name: (type_identifier) @AlabasterDefinition)
+
+;; decorators
+(decorator
+  ((identifier) @AlabasterDefinition)?
+  (call_expression
+    function: (identifier) @AlabasterDefinition)?)
+
+;; method calls 
+(call_expression
+  function: (member_expression 
+    property: (property_identifier) @AlabasterDefinition))
+
+;; type arguments
+(type_arguments
+  (_) @AlabasterDefinition)
+
 (undefined) @AlabasterConstant
