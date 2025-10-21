481910715c46b83b9bf50bb9402d176391fb3017
Alex Sokol <y9san9@gmail.com>
2025-10-22 01:52:55 +0300

    Proper Kotlin Support (#26)
    * Added Kotlin object declaration support
    
    * Proper Kotlin support
    
    Added syntax highlighting for various literal types and comments.

--- after/queries/kotlin/highlights.scm
+++ after/queries/kotlin/highlights.scm
@@ -6,4 +6,19 @@
 (class_declaration
   (type_identifier) @AlabasterDefinition)
 
+(object_declaration
+  (type_identifier) @AlabasterDefinition)
+
+(string_literal) @AlabasterString
+
+(boolean_literal) @AlabasterConstant
+(integer_literal) @AlabasterConstant
+(long_literal) @AlabasterConstant
+(unsigned_literal) @AlabasterConstant
+(real_literal) @AlabasterConstant
+
+(line_comment) @AlabasterHashbang
+(multiline_comment) @Comment
+
+
 (modifiers (annotation (user_type (type_identifier) @AlabasterPunctuation)))
